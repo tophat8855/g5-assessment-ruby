900034ce85d08d155c65e6716ed6530e769f372f
tophat8855 <hrfarley@gmail.com>
2014-12-18 10:28:19 -0800

    accessed first poem and started the skeleton of a hash

--- poems.rb
+++ poems.rb
@@ -1,11 +1,17 @@
-# glob the files
 # go through each file
 # parse them by lines (look up how to parse .txt file)
 # make a hash
 # second line is key to hash
 # first line (aka title) is value of the second and is key to hash that
 # points to hash of verse and line amounts.
+# need to take the '\n' off of each line
 
 datadirs = File.join("**", "data", "**", "*.txt")
 
-puts Dir.glob(datadirs)[0]
+file = Dir.glob(datadirs)[0]
+
+poem = IO.readlines(file)
+
+poem_data_hash = { poem[1] => {poem[0] => "thing"}}
+
+puts poem_data_hash
