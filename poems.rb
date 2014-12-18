ff1609ebb92f92bbb8ac9e2b29a096596b419852
tophat8855 <hrfarley@gmail.com>
2014-12-18 11:03:46 -0800

    gives us a hash of the first poem

--- poems.rb
+++ poems.rb
@@ -3,15 +3,32 @@
 # make a hash
 # second line is key to hash
 # first line (aka title) is value of the second and is key to hash that
-# points to hash of verse and line amounts.
+# points to hash of verse and line amounts (need to count those in an each loop).
+# if I cut off the first 3 lines of the txt file, I'm left with the verses
 # need to take the '\n' off of each line
 
 datadirs = File.join("**", "data", "**", "*.txt")
 
-file = Dir.glob(datadirs)[0]
+file = Dir.glob(datadirs)[6]
 
 poem = IO.readlines(file)
 
-poem_data_hash = { poem[1] => {poem[0] => "thing"}}
+number_of_lines_in_file = IO.readlines(file).count
+puts number_of_lines_in_file
+
+
+#this will count the number of empty lines in the file
+#number of empty lines = number of verses
+empty_line_counter = 0
+poem.each do |line|
+  if line == "\n"
+    empty_line_counter += 1
+  end
+end
+
+poem_line_count = number_of_lines_in_file - 2 - empty_line_counter
+
+
+poem_data_hash = { poem[1] => {poem[0] => {verses: empty_line_counter, lines: poem_line_count}}}
 
 puts poem_data_hash
