fe2e7fb505cc545eb50ca721fe5249678aa67898
tophat8855 <hrfarley@gmail.com>
2014-12-19 11:41:31 -0800

    fixed arrayness into hashness

--- poems.rb
+++ poems.rb
@@ -5,6 +5,7 @@
 
 all_the_poems = File.join("**", "data", "**", "*.txt")
 
+hash_of_poems = {}
 array_of_poems = []
 
 Dir.glob(all_the_poems).each do |file|
@@ -25,14 +26,22 @@ Dir.glob(all_the_poems).each do |file|
 
   poem_line_count = number_of_lines_in_file - number_of_lines_in_header - empty_line_counter
 
-  poem_data_hash = { poem[1].gsub("\n","") => { poem[0].gsub("\n","") => { verses: empty_line_counter, lines: poem_line_count } } }
+  author = poem[1].gsub("\n","")
+  title = poem[0].gsub("\n","")
+  authors = hash_of_poems.keys
 
-  array_of_poems << poem_data_hash
+  if authors.include?(author)
+    hash_of_poems[author][title] = { verses: empty_line_counter, lines: poem_line_count }
+  else
+    hash_of_poems[author] = { title => { verses: empty_line_counter, lines: poem_line_count } }
+  end
+
+  hash_of_poems
 end
 
-merge_hashes = array_of_poems.each_with_object({}) do |author, hash|
-  key,value = author.shift
-  (hash[key] ||= []) << value #this is where the unruly array is being made
-end
+# merge_hashes = array_of_poems.each_with_object({}) do |author, hash|
+#   key,value = author.shift
+#   (hash[key] ||= []) << value #this is where the unruly array is being made
+# end
 
-puts merge_hashes
+puts hash_of_poems
